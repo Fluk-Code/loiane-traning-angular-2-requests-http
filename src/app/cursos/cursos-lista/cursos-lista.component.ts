a1e4db177a95a3886e8fea61500acfb6e691d774
Felipe Ferreira <ferreirafelipericardo@gmail.com>
2021-09-15 14:24:28 -0300

    #120: Injetado cursos.service em cursos-lista.component e comusimado metodo listar em ngOnInt

--- src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CursosService } from '../cursos.service';
+import { Cursos } from '../curso';
+
 @Component({
   selector: 'app-cursos-lista',
   templateUrl: './cursos-lista.component.html',
-  styleUrls: ['./cursos-lista.component.scss']
+  styleUrls: ['./cursos-lista.component.scss'],
+  preserveWhitespaces: true
 })
 export class CursosListaComponent implements OnInit {
 
-  constructor() { }
+  cursos: Cursos
+
+  constructor(
+    private cursoService: CursosService
+  ) { }
 
   ngOnInit(): void {
+    this.cursoService.listar()
+      .subscribe(response => this.cursos = response)
   }
 
 }
