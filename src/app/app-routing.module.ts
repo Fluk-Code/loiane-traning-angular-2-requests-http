2eb466788b9e97200272ae7ed13af8f9e8ff2462
Felipe Ferreira <ferreirafelipericardo@gmail.com>
2021-09-15 12:43:07 -0300

    #118: Criado modulo cursos + componete cursos-lista + rota lazy load cursos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'cursos'
+  },
+  {
+    path: 'cursos',
+    loadChildren: () => import('./cursos/cursos.module')
+      .then(module => module.CursosModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
