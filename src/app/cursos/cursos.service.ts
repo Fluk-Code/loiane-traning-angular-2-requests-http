d566fd012cfa2dcda6bf906d3f509eed5ce1a7cd
Felipe Ferreira <ferreirafelipericardo@gmail.com>
2021-09-16 12:37:32 -0300

    121: refatorado em cursos.service o atributo API para buscar caminho url nas variaveis de ambiente

--- src/app/cursos/cursos.service.ts
+++ src/app/cursos/cursos.service.ts
@@ -4,13 +4,14 @@ import { HttpClient } from '@angular/common/http'
 import { tap } from 'rxjs/operators'
 
 import { Cursos } from './curso';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CursosService {
 
-  private readonly API = ' http://localhost:3000/cursos';
+  private readonly API = `${environment.API}cursos`
 
   constructor(
     private http: HttpClient
