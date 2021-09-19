61a56f510ff5752f31f3e651836273c7119e4dd2
Felipe Ferreira <ferreirafelipericardo@gmail.com>
2021-09-19 10:17:58 -0300

    136: Criado servidor backend com nodejs para upload de arquivos

--- src/app/upload-file/upload-file/upload-file.component.ts
+++ src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { UploadFileService } from '../upload-file.service';
 
 @Component({
   selector: 'app-upload-file',
@@ -7,7 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadFileComponent implements OnInit {
 
-  constructor() { }
+  files: Set<File>
+  API: string
+
+  constructor(
+    private uploadFileService: UploadFileService
+  ) { 
+    this.API = environment.API
+  }
 
   ngOnInit(): void {
   }
@@ -15,11 +24,24 @@ export class UploadFileComponent implements OnInit {
   onChange(event) {
     const selectedFiles = <FileList>event.srcElement.files
     const fileNames = []
+    this.files = new Set()
+
     for (let i=0; i < selectedFiles.length; i++){
       fileNames.push(selectedFiles[i].name)
+      this.files.add(selectedFiles[i])
     }
 
     document.getElementById('arquivosSelecionados').innerHTML = fileNames.join(', ')
+  }
+
+  onUpload() {
+    if (this.files && this.files.size > 0){
+      this.uploadFileService.upload(this.files, `${this.API}upload`)
+        .subscribe(
+          response => console.log('upload concluido'),
+          error => console.error(error)
+        )
+    }
   }
 
 }
