6701a4dc317f4a90fd1b1b2cf40dc3e0882f71cc
Felipe Ferreira <ferreirafelipericardo@gmail.com>
2021-09-19 11:53:21 -0300

    140: HTTP: Refatorado progresso upload para operators customizados RxJS

--- src/app/upload-file/upload-file/upload-file.component.ts
+++ src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,5 +1,6 @@
 import { HttpEvent, HttpEventType } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { filterResponse, uploadProgress } from 'src/app/shared/rxjs/rxjs-operators';
 import { environment } from 'src/environments/environment';
 import { UploadFileService } from '../upload-file.service';
 
@@ -43,20 +44,31 @@ export class UploadFileComponent implements OnInit {
   onUpload() {
     if (this.files && this.files.size > 0){
       this.uploadFileService.upload(this.files, `${this.API}upload`)
-        .subscribe(
-          (event: HttpEvent<Object>) => {
-            if (event.type === HttpEventType.Response) {
-              console.log('upload concluido')
-            }
-            else if (event.type === HttpEventType.UploadProgress) {
-              const percentDone = Math.round((event.loaded * 100)/ event.total)
-              this.progress = percentDone
-              console.log('Progresso: ', percentDone)
-            }
-            console.log(event)
-          },
-          error => console.error(error)
+        .pipe(
+          uploadProgress(progress => {
+            console.log(progress)
+            this.progress = progress
+          }),
+          filterResponse()
         )
+        // .subscribe(
+        //   (event: HttpEvent<Object>) => {
+        //     if (event.type === HttpEventType.Response) {
+        //       console.log('upload concluido')
+        //     }
+        //     else if (event.type === HttpEventType.UploadProgress) {
+        //       const percentDone = Math.round((event.loaded * 100)/ event.total)
+        //       this.progress = percentDone
+        //       console.log('Progresso: ', percentDone)
+        //     }
+        //     console.log(event)
+        //   },
+        //   error => console.error(error)
+        // )
+          .subscribe(
+            response => console.log('Upload Concluido'),
+            error => console.error(error)
+          )
     }
   }
 
