658f1882f364c78c96429ee0883115c7430811e0
Felipe Ferreira <ferreirafelipericardo@gmail.com>
2021-09-19 09:56:19 -0300

    136: Criado servidor backend com nodejs para upload de arquivos

---
+++ server/src/server.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import multiparty from 'connect-multiparty'
+import cors from 'cors'
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const corsOptions = {
+  origin: '*',
+  optionsSucessStatus: 200
+}
+
+const multipartyMiddleware = multiparty({ uploadDir: './uploads' })
+app.post('/upload', multipartyMiddleware, (req, res) => {
+  const files = req.files
+
+  console.log(files)
+
+  res.status(200).json({ message: files })
+})
+
+app.use((err, req, res, next) => res.json({ error: err.message }))
+
+app.use(cors(corsOptions))
+
+app.listen(3000, () => console.log('SERVIDOR ONLINE NA PORTA 3000'))
